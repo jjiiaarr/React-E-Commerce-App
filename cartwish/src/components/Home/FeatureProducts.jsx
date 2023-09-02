c9a5a3023bcf616465649c7341026fcfe7d48ab3
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-02 18:12:01 +0800

    create product card component for feature products and style

--- cartwish/src/components/Home/FeatureProducts.jsx
+++ cartwish/src/components/Home/FeatureProducts.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import "./FeatureProducts.css";
+import ProductCard from "./ProductCard";
 
 const FeatureProducts = () => {
   return (
     <section className="feature_products">
       <h2>Featured Products</h2>
       <div className="align_center feature_products_list">
-        <article className="product_card">Product</article>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
   );
