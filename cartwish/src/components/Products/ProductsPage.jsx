5a66dd6d8d7dc6766f6a8618f2ca44ae6f475075
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-02 18:16:52 +0800

    create products page component for products

---
+++ cartwish/src/components/Products/ProductsPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./ProductsPage.css";
+
+const ProductsPage = () => {
+  return (
+    <section className="products_page">
+      <aside className="products_sidebar">Sidebar</aside>
+
+      <section className="products_list_section">Products List</section>
+    </section>
+  );
+};
+
+export default ProductsPage;
