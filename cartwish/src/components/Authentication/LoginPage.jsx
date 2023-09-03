d8d91580232789e1517de9e6d557d27e3a5adbf7
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-03 17:25:52 +0800

    handling form using Ref hook

--- cartwish/src/components/Authentication/LoginPage.jsx
+++ cartwish/src/components/Authentication/LoginPage.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./LoginPage.css";
 
 const LoginPage = () => {
+  const nameRef = useRef(null);
+  const phoneRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      name: "",
+      phone: 0,
+    };
+    user.name = nameRef.current.value;
+    user.phone = parseInt(phoneRef.current.value);
+    console.log(user);
+  };
+
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form className="authentication_form" onSubmit={handleSubmit}>
         <h2>Login Form</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="name">Name</label>
             <input
               type="text"
+              ref={nameRef}
               id="name"
               className="form_text_input"
               placeholder="Enter your name"
@@ -21,6 +36,7 @@ const LoginPage = () => {
             <label htmlFor="phone">Phone Number</label>
             <input
               type="number"
+              ref={phoneRef}
               id="phone"
               className="form_text_input"
               placeholder="Enter your phone number"
