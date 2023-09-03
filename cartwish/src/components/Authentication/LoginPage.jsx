f03ae5c34b65337f8cae2d2e3c66d504b5bf481e
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-03 18:24:26 +0800

    Handling form using state hook

--- cartwish/src/components/Authentication/LoginPage.jsx
+++ cartwish/src/components/Authentication/LoginPage.jsx
@@ -1,19 +1,15 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 import "./LoginPage.css";
 
 const LoginPage = () => {
-  const nameRef = useRef(null);
-  const phoneRef = useRef(null);
+  const [user, setUser] = useState({
+    name: "",
+    phone: "",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const user = {
-      name: "",
-      phone: 0,
-    };
-    user.name = nameRef.current.value;
-    user.phone = parseInt(phoneRef.current.value);
     console.log(user);
   };
 
@@ -26,20 +22,24 @@ const LoginPage = () => {
             <label htmlFor="name">Name</label>
             <input
               type="text"
-              ref={nameRef}
               id="name"
               className="form_text_input"
               placeholder="Enter your name"
+              onChange={(e) => setUser({ ...user, name: e.target.value })}
+              value={user.name}
             />
           </div>
           <div>
             <label htmlFor="phone">Phone Number</label>
             <input
               type="number"
-              ref={phoneRef}
               id="phone"
               className="form_text_input"
               placeholder="Enter your phone number"
+              onChange={(e) =>
+                setUser({ ...user, phone: parseInt(e.target.value) })
+              }
+              value={user.phone}
             />
           </div>
           <button type="submit" className="search_button form_submit">
