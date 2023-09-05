9b4b93ccf816ee201e74187151432b6dee9ddde9
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-05 19:38:46 +0800

    add loading skeleton to product card

--- cartwish/src/components/Products/ProductsList.jsx
+++ cartwish/src/components/Products/ProductsList.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import "./ProductsList.css";
 import ProductCard from "./ProductCard";
 import useData from "../../hooks/useData";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 const ProductsList = () => {
-  const { data, error } = useData("/products");
+  const { data, error, isLoading } = useData("/products");
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <section className="products_list_section">
       <header className="align_center products_list_header">
@@ -20,6 +22,7 @@ const ProductsList = () => {
       </header>
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
         {data?.products &&
           data.products.map((product) => (
             <ProductCard
