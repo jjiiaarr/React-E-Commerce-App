7968398a4daf71b2340731ea8ed6bb494d0e444e
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-05 18:40:57 +0800

    create fetching custom hook

--- cartwish/src/components/Products/ProductsList.jsx
+++ cartwish/src/components/Products/ProductsList.jsx
@@ -1,19 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "./ProductsList.css";
 import ProductCard from "./ProductCard";
-import apiClient from "../../utils/api-client";
+import useData from "../../hooks/useData";
 
 const ProductsList = () => {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get("/products")
-      .then((res) => setProducts(res.data.products))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { data, error } = useData("/products");
   return (
     <section className="products_list_section">
       <header className="align_center products_list_header">
@@ -28,18 +20,19 @@ const ProductsList = () => {
       </header>
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
-        {products.map((product) => (
-          <ProductCard
-            key={product._id}
-            id={product._id}
-            image={product.images[0]}
-            price={product.price}
-            title={product.title}
-            rating={product.reviews.rating}
-            ratingCounts={product.reviews.counts}
-            stock={product.stock}
-          />
-        ))}
+        {data?.products &&
+          data.products.map((product) => (
+            <ProductCard
+              key={product._id}
+              id={product._id}
+              image={product.images[0]}
+              price={product.price}
+              title={product.title}
+              rating={product.reviews.rating}
+              ratingCounts={product.reviews.counts}
+              stock={product.stock}
+            />
+          ))}
       </div>
     </section>
   );
--- cartwish/src/components/Products/ProductsSidebar.jsx
+++ cartwish/src/components/Products/ProductsSidebar.jsx
@@ -1,35 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import apiClient from "../../utils/api-client";
 import "./ProductsSidebar.css";
-import rocket from "../../assets/rocket.png";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
+import useData from "../../hooks/useData";
 
 const ProductsSidebar = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get("/category")
-      .then((res) => setCategories(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { data: categories, error } = useData("/category");
   return (
     <aside className="products_sidebar">
       <h2>Category</h2>
       <div className="category_links">
         {error && <em className="form_error">{error}</em>}
-        {categories.map((category) => (
-          <LinkWithIcon
-            key={category._id}
-            id={category._id}
-            title={category.name}
-            link={`products?category=${category.name}`}
-            emoji={`http://localhost:5000/category/${category.image}`}
-            sidebar={true}
-          />
-        ))}
+        {categories &&
+          categories.map((category) => (
+            <LinkWithIcon
+              key={category._id}
+              id={category._id}
+              title={category.name}
+              link={`products?category=${category.name}`}
+              emoji={`http://localhost:5000/category/${category.image}`}
+              sidebar={true}
+            />
+          ))}
       </div>
     </aside>
   );
