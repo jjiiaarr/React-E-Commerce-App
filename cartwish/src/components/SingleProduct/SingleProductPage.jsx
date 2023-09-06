2b2193753e3f9871f68bc3493ee477d123549a1b
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-06 21:00:09 +0800

    add loader component

--- cartwish/src/components/SingleProduct/SingleProductPage.jsx
+++ cartwish/src/components/SingleProduct/SingleProductPage.jsx
@@ -4,6 +4,7 @@ import "./SingleProductPage.css";
 import QuantityInput from "./QuantityInput";
 import { useParams } from "react-router-dom";
 import useData from "../../hooks/useData";
+import Loader from "./../Common/Loader";
 
 const SingleProductPage = () => {
   const [selectedImage, setSelectedImage] = useState(0);
@@ -12,13 +13,15 @@ const SingleProductPage = () => {
   const { data: product, error, isLoading } = useData(`/products/${id}`);
   return (
     <section className="align_center single_product">
+      {error && <em className="form_error">{error}</em>}
+      {isLoading && <Loader />}
       {product && (
         <>
           <div className="align_center">
             <div className="single_product_thumbnails">
               {product.images.map((image, index) => (
                 <img
-                  src={image}
+                  src={`http://localhost:5000/products/${image}`}
                   alt={product.title}
                   className={selectedImage === index ? "selected_image" : ""}
                   onClick={() => setSelectedImage(index)}
@@ -26,7 +29,7 @@ const SingleProductPage = () => {
               ))}
             </div>
             <img
-              src={product.images[selectedImage]}
+              src={`http://localhost:5000/products/${product.images[selectedImage]}`}
               alt={product.title}
               className="single_product_display"
             />
