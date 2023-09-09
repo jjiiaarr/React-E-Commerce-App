7196dc0c65eec94c287580d9f835777a5cc1c276
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-09 20:28:42 +0800

    add increase/decrease functionality of product quantity

--- cartwish/src/App.jsx
+++ cartwish/src/App.jsx
@@ -9,7 +9,9 @@ import { getJwt, getUser } from "./services/userServices";
 import setAuthToken from "./utils/setAuthToken";
 import {
   addToCartAPI,
+  decreaseProductAPI,
   getCartAPI,
+  increaseProductAPI,
   removeFromCartAPI,
 } from "./services/cartServices";
 import "react-toastify/dist/ReactToastify.css";
@@ -66,6 +68,32 @@ const App = () => {
     });
   };
 
+  const updateCart = (type, id) => {
+    const oldCart = [...cart];
+    const updatedCart = [...cart];
+    const productIndex = updatedCart.findIndex(
+      (item) => item.product._id === id
+    );
+    if (type === "increase") {
+      updatedCart[productIndex].quantity += 1;
+      setCart(updatedCart);
+
+      increaseProductAPI(id).catch((err) => {
+        toast.error("Something went wrong");
+        setCart(oldCart);
+      });
+    }
+    if (type === "decrease") {
+      updatedCart[productIndex].quantity -= 1;
+      setCart(updatedCart);
+
+      decreaseProductAPI(id).catch((err) => {
+        toast.error("Something went wrong");
+        setCart(oldCart);
+      });
+    }
+  };
+
   const getCart = () => {
     getCartAPI()
       .then((res) => {
@@ -84,7 +112,9 @@ const App = () => {
 
   return (
     <UserContext.Provider value={user}>
-      <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      <CartContext.Provider
+        value={{ cart, addToCart, removeFromCart, updateCart }}
+      >
         <div className="app">
           <Navbar />
           <main>
