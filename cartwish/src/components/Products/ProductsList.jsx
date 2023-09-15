caf7d383086e4ffdac0f879016198f1679a8b7fe
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-09-15 14:11:41 +0800

    add sorting product list

--- cartwish/src/components/Products/ProductsList.jsx
+++ cartwish/src/components/Products/ProductsList.jsx
@@ -9,6 +9,8 @@ import Pagination from "../Common/Pagination";
 
 const ProductsList = () => {
   const [page, setPage] = useState(1);
+  const [sortBy, setSortBy] = useState("");
+  const [sortedProducts, setSortedProducts] = useState([]);
   const [search, setSearch] = useSearchParams();
   const category = search.get("category");
   const searchQuery = search.get("search");
@@ -54,11 +56,39 @@ const ProductsList = () => {
 
     return () => window.removeEventListener("scroll", handleScroll);
   }, [data, isLoading]);
+
+  useEffect(() => {
+    if (data && data.products) {
+      const products = [...data.products];
+
+      if (sortBy === "price desc") {
+        setSortedProducts(products.sort((a, b) => b.price - a.price));
+      } else if (sortBy === "price asc") {
+        setSortedProducts(products.sort((a, b) => a.price - b.price));
+      } else if (sortBy === "rate desc") {
+        setSortedProducts(
+          products.sort((a, b) => b.reviews.rate - a.reviews.rate)
+        );
+      } else if (sortBy === "rate asc") {
+        setSortedProducts(
+          products.sort((a, b) => a.reviews.rate - b.reviews.rate)
+        );
+      } else {
+        setSortedProducts(products);
+      }
+    }
+  }, [sortBy, data]);
+
   return (
     <section className="products_list_section">
       <header className="align_center products_list_header">
         <h2>Products</h2>
-        <select name="sort" id="" className="products_sorting">
+        <select
+          name="sort"
+          id=""
+          className="products_sorting"
+          onChange={(e) => setSortBy(e.target.value)}
+        >
           <option value="">Relevance</option>
           <option value="price desc">Price HIGH to LOW</option>
           <option value="price asc">Price LOW to HIGH</option>
@@ -69,7 +99,7 @@ const ProductsList = () => {
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
         {data?.products &&
-          data.products.map((product) => (
+          sortedProducts.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
         {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
